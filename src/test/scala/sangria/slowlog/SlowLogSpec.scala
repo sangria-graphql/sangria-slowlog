a89e389ac87d7dbdfb7f11484055050be1877e04
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2017-06-18 00:35:59 +0200

    Added extensions test

--- src/test/scala/sangria/slowlog/SlowLogSpec.scala
+++ src/test/scala/sangria/slowlog/SlowLogSpec.scala
@@ -2,6 +2,8 @@ package sangria.slowlog
 
 import java.util.concurrent.TimeUnit
 
+import org.json4s.JsonAST.{JInt, JString, JValue}
+
 import language.postfixOps
 import sangria.slowlog.util.FutureResultSupport
 import org.scalatest.{Matchers, OptionValues, WordSpec}
@@ -10,11 +12,11 @@ import sangria.schema._
 import sangria.macros._
 import sangria.marshalling.ScalaInput
 
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import sangria.marshalling.playJson._
+import sangria.marshalling.json4s.native._
+import org.json4s.native.JsonMethods._
 import sangria.slowlog.util._
 
 class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with StringMatchers with OptionValues {
@@ -50,7 +52,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
   val PersonType = ObjectType("Person", interfaces[Unit, Person](NamedType), fields[Unit, Person](
     Field("pets", OptionType(ListType(OptionType(PetType))),
       arguments = LimitArg :: Nil,
-      resolve = _.value.pets),
+      resolve = c ⇒ c.withArgs(LimitArg)(limit ⇒ c.value.pets.map(_.take(limit)))),
     Field("favouritePet", PetType, resolve = _.value.pets.flatMap(_.headOption.flatMap(identity)).get),
     Field("favouritePetList", ListType(PetType), resolve = _.value.pets.getOrElse(Nil).flatMap(x ⇒ x).toSeq),
     Field("favouritePetOpt", OptionType(PetType), resolve = _.value.pets.flatMap(_.headOption.flatMap(identity))),
@@ -113,7 +115,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
 
   "SlowLog" should {
     "enrich the query and separate operation" in {
-      val vars = ScalaInput.scalaInput(Map("limit" → 10))
+      val vars = ScalaInput.scalaInput(Map("limit" → 30))
       var enrichedQuery: Option[String] = None
 
       Executor.execute(schema, mainQuery,
@@ -125,7 +127,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
       removeTime(enrichedQuery.value, "ms") should equal (
         """# [Execution Metrics] duration: 0ms, validation: 0ms, reducers: 0ms
           |#
-          |# $limit = 10
+          |# $limit = 30
           |query Test($limit: Int!) {
           |  # [Person] count: 1, time: 0ms
           |  __typename
@@ -136,7 +138,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
           |
           |  # [Person] count: 1, time: 0ms
           |  #
-          |  # $limit = 10
+          |  # $limit = 30
           |  pets(limit: $limit) {
           |    ... on Cat {
           |      # [Cat] count: 20, min: 0ms, max: 0ms, mean: 0ms, p75: 0ms, p95: 0ms, p99: 0ms
@@ -179,7 +181,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
     }
 
     "enrich only relevant parts of the query" in {
-      val vars = ScalaInput.scalaInput(Map("limit" → 10))
+      val vars = ScalaInput.scalaInput(Map("limit" → 30))
       var enrichedQuery: Option[String] = None
 
       Executor.execute(schema, mainQuery,
@@ -198,7 +200,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
           |
           |# [Execution Metrics] duration: 0ms, validation: 0ms, reducers: 0ms
           |#
-          |# $limit = 10
+          |# $limit = 30
           |query Test($limit: Int!) {
           |  # [Person] count: 1, time: 0ms
           |  __typename
@@ -209,7 +211,7 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
           |
           |  # [Person] count: 1, time: 0ms
           |  #
-          |  # $limit = 10
+          |  # $limit = 30
           |  pets(limit: $limit) {
           |    ... on Cat {
           |      # [Cat] count: 20, min: 0ms, max: 0ms, mean: 0ms, p75: 0ms, p95: 0ms, p99: 0ms
@@ -281,8 +283,70 @@ class SlowLogSpec extends WordSpec with Matchers with FutureResultSupport with S
           |  name
           |}""".stripMargin) (after being strippedOfCarriageReturns)
     }
+
+    "add extensions" in {
+      val vars = ScalaInput.scalaInput(Map("limit" → 3))
+
+      val res = Executor.execute(schema, mainQuery,
+        root = bob,
+        operationName = Some("Test"),
+        variables = vars,
+        middleware = SlowLog.extension :: Nil).await
+
+      removeTime(res, "ms", "Ms") should be (parse(
+        """{
+          |  "data": {
+          |    "__typename": "Person",
+          |    "name": "Bob",
+          |    "pets": [
+          |      {
+          |        "name": "Garfield",
+          |        "meows": false
+          |      },
+          |      {
+          |        "name": "Garfield",
+          |        "meows": false
+          |      },
+          |      {
+          |        "name": "Garfield",
+          |        "meows": false
+          |      }
+          |    ]
+          |  },
+          |  "extensions": {
+          |    "metrics": {
+          |      "executionMs": 0,
+          |      "validationMs": 0,
+          |      "reducersMs": 0,
+          |      "query": "query Foo {\n  friends {\n    ...Name\n    ...Name2\n  }\n}\n\n# [Execution Metrics] duration: 0ms, validation: 0ms, reducers: 0ms\n#\n# $limit = 3\nquery Test($limit: Int!) {\n  # [Person] count: 1, time: 0ms\n  __typename\n\n  # [Person] count: 1, time: 0ms\n  name\n  ...Name1\n\n  # [Person] count: 1, time: 0ms\n  #\n  # $limit = 3\n  pets(limit: $limit) {\n    ... on Cat {\n      # [Cat] count: 3, min: 0ms, max: 0ms, mean: 0ms, p75: 0ms, p95: 0ms, p99: 0ms\n      name\n\n      # [Cat] count: 3, min: 0ms, max: 0ms, mean: 0ms, p75: 0ms, p95: 0ms, p99: 0ms\n      meows\n      ...Name\n    }\n    ... on Dog {\n      ...Name1\n      ...Name1\n      foo: name\n      barks\n    }\n  }\n}\n\n# [usages]\n# * pets\nfragment Name on Named {\n  # [pets.name Cat] count: 3, min: 0ms, max: 0ms, mean: 0ms, p75: 0ms, p95: 0ms, p99: 0ms\n  name\n  ...Name1\n}\n\n# [usages]\n# * (query root)\n# * pets.(Name)\nfragment Name1 on Named {\n  ... on Person {\n    # [name Person] count: 1, time: 0ms\n    name\n  }\n}\n\nfragment Name2 on Named {\n  name\n}",
+          |      "types": {
+          |        "Cat": {
+          |          "name": {"count": 3, "minMs": 0, "maxMs": 0, "meanMs": 0, "p75Ms": 0, "p95Ms": 0, "p99Ms": 0},
+          |          "meows": {"count": 3, "minMs": 0, "maxMs": 0, "meanMs": 0, "p75Ms": 0, "p95Ms": 0, "p99Ms": 0}
+          |        },
+          |        "Person": {
+          |          "pets": {"count": 1, "minMs": 0, "maxMs": 0, "meanMs": 0, "p75Ms": 0, "p95Ms": 0, "p99Ms": 0},
+          |          "__typename": {"count": 1, "minMs": 0, "maxMs": 0, "meanMs": 0, "p75Ms": 0, "p95Ms": 0, "p99Ms": 0},
+          |          "name": {"count": 1, "minMs": 0, "maxMs": 0, "meanMs": 0, "p75Ms": 0, "p95Ms": 0, "p99Ms": 0}
+          |        }
+          |      }
+          |    }
+          |  }
+          |}""".stripMargin))
+    }
   }
 
-  def removeTime(query: String, unit: String) =
+  def removeTime(json: JValue, unit: String, fieldUnit: String): JValue =
+    json.transformField {
+      case (en @ "extensions", ev) ⇒
+        en → ev.transformField {
+          case (n @ "query", JString(s)) ⇒
+            n → JString(removeTime(s, unit))
+          case (n, JInt(i)) if n endsWith fieldUnit ⇒
+            n → JInt(0)
+        }
+    }
+
+  def removeTime(query: String, unit: String): String =
     query.replaceAll("\\d+" + unit, "0" + unit)
 }
